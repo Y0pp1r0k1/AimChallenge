9baf9ae89488f4547354e6293ef60df476b1ab27
進藤仁貴 <ys08260002@outlook.jp>
2023-04-25 18:38:38 +0900

    first commit

---
+++ aim.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('.jQuery').click(function() {
+        $('.jQuery').css('display','none');
+    });
+});
+
+$(function() {
+    $('.manual-title1').click(function() {
+        $('.manual-tips1').fadeToggle(1000);
+    });
+});
+
+$(function() {
+    $('.manual-title2').click(function() {
+        $('.manual-tips2').fadeToggle(1000);
+    });
+});
